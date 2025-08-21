4ae834467c60e99f74d6b12290346b2d8689644c
Parth Bhadra <66181241+Parthbhadra07@users.noreply.github.com>
2025-08-21 15:36:18 +0530

    gallery change

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -10,46 +10,46 @@ export default function Gallery() {
 
             <div class="flex flex-col gap-4">
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/badroom.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/badroom.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery2.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery2.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery.jpg" alt="" />
-                </div>
-            </div>
-            <div class="flex flex-col gap-4">
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/swiper1.jpg" alt="" />
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery11.jpg" alt="" />
-                </div>
-                <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/living.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery.jpg" alt="" />
                 </div>
             </div>
             <div class="flex flex-col gap-4">
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery14.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/swiper1.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery15.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery11.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery12.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="../image/living.jpg" alt="" />
                 </div>
             </div>
             <div class="flex flex-col gap-4">
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery7.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery14.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery11.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery15.jpg" alt="" />
                 </div>
                 <div>
-                    <img class="h-auto max-w-full rounded-lg" src="/image/gallery3.jpg" alt="" />
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery12.jpg" alt="" />
+                </div>
+            </div>
+            <div class="flex flex-col gap-4">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery7.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery11.jpg" alt="" />
+                </div>
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="./image/gallery3.jpg" alt="" />
                 </div>
             </div>
         </div>
